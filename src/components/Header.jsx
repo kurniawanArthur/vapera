3b0e4032d9196c31591a240b30a0337b0cca634d
kurniawanArthur <krnwnarthur@gmail.com>
2023-07-16 23:36:23 +0700

    major push

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,7 +1,6 @@
 const Header = () => {
   return (
-    <div>
-      <img className="logo-image" src="./vapera-logo.png" alt="" />
+    <div className="header-container">
       <ul className="header-ul">
         <li>Home</li>
         <li>Products</li>
--- src/components/LandingPage.jsx
+++ src/components/LandingPage.jsx
@@ -1,11 +1,15 @@
+import mainLogo from "../img/vapera-logo.png";
 const LandingPage = () => {
   return (
     <>
       <div className="landing-page">
         <div className="content-container">
           <div className="introduction-text">
-            <h1>Vapera</h1>
-            <p>
+            <div className="title-logo-box">
+              <img width={100} className="img-logo" src={mainLogo} alt="" />
+              <h1 className="title">VAPERA</h1>
+            </div>
+            <p className="text-title">
               Lorem ipsum dolor, sit amet consectetur adipisicing elit. Esse
               recusandae aperiam officia voluptatibus facere vitae deserunt
               doloremque, reiciendis accusamus aspernatur deleniti rem porro,
