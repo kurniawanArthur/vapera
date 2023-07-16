3179a38f0ab0bb2d3e9cb0fc69a6dbb5c13b0e83
kurniawanArthur <krnwnarthur@gmail.com>
2023-07-16 22:41:07 +0700

    first commit

---
+++ src/components/Header.jsx
@@ -0,0 +1,15 @@
+const Header = () => {
+  return (
+    <div>
+      <img className="logo-image" src="./vapera-logo.png" alt="" />
+      <ul className="header-ul">
+        <li>Home</li>
+        <li>Products</li>
+        <li>Event</li>
+        <li>Our Team</li>
+      </ul>
+    </div>
+  );
+};
+
+export default Header;
---
+++ src/components/LandingPage.jsx
@@ -0,0 +1,31 @@
+const LandingPage = () => {
+  return (
+    <>
+      <div className="landing-page">
+        <div className="content-container">
+          <div className="introduction-text">
+            <h1>Vapera</h1>
+            <p>
+              Lorem ipsum dolor, sit amet consectetur adipisicing elit. Esse
+              recusandae aperiam officia voluptatibus facere vitae deserunt
+              doloremque, reiciendis accusamus aspernatur deleniti rem porro,
+              laudantium voluptates harum quaerat mollitia voluptatem a tempore
+              fugit animi exercitationem quis error quos! In deserunt magnam nam
+              reprehenderit aliquam laudantium labore quo, eveniet ut tenetur
+              natus?
+            </p>
+            <button>ini tombol</button>
+          </div>
+          <img
+            className="img-landing"
+            src="Https://source.unsplash.com/random/540x720/?vape"
+            alt=""
+            srcset=""
+          />
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default LandingPage;
